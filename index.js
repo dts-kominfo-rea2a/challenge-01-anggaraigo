dfb5cb964ce7a244b406d3d7adffea4da1784bf2
igo <igoanggaraa@gmail.com>
2022-05-29 21:30:58 +0700

    Tantangan: JS Struktur Data

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+
+const favoriteColorMonica = ["Yellow","Pink","White","Purple"]
+const favoriteColorWendy  = ["Blue", "Black", "Grey"]
+
+const educationMonica = [
+    {name: "SD 01", city: "Jakarta", graduate: 2016},
+    {name: "SMP 02", city: "Jakarta", graduate: 2019},
+    {name: "SMA 03", city: "Tangerang"}
+];
+
+const educationWendy = [
+    {name: "SD 01", city: "Jakarta", graduate: 2016},
+    {name: "SMP 02", city: "Jakarta", graduate: 2019},
+    {name: "SMA 03", city: "Tangerang"}
+];
+
+const favoriteRestaurantMonica = ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]
+const favoriteRestaurantWendy = ["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]
+
+const firstUser = {
+    name:"Monica",
+    gender:"Female",
+    age:17,
+    email:"monica@dingdong.com",
+    favoriteColor: [...new Set(favoriteColorMonica)],
+    isHavePet:"Yes",
+    education:educationMonica,
+    favoriteRestaurant: [...new Set(favoriteRestaurantMonica)]
+};
+
+const secondUser = {
+    name:"Wendy",
+    gender:"Male",
+    age:23,
+    email:"wendy@dingdong.com",
+    favoriteColor:[...new Set(favoriteColorWendy)],
+    isHavePet:"No",
+    education:educationWendy,
+    favoriteRestaurant: [...new Set(favoriteRestaurantWendy)]
+
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [];
+const users = [firstUser,secondUser];
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
